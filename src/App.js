76f27bd675c6a52b290ad23a222f8606b94de5e4
Guillaume Ongenae <guillaume.ongenae@gmail.com>
2024-03-24 15:46:09 +0100

    Remove konami code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import Konami from "konami";
-import { useEffect } from "react";
-
 import profile from "./profile.jpg";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    // eslint-disable-next-line
-    new Konami(() => alert("Joyeux Noël et Bonne année 2023 !"));
-  }, []);
-
   return (
     <div className="app">
       <section>
